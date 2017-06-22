fb7b6481a30e33c2cc2379ac5bd166b8e6ebb1d9
mbazhlekova <maria.bazhlekova@gmail.com>
2017-06-21 21:00:12 -0500

    added search endpoint

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,25 @@
+var dotenv = require('dotenv');
+dotenv.load();
+
+const api_key = process.env.API_KEY;
+const searchUrl = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key;
+
 var express = require('express');
+var request = require('request');
+
 var router = express.Router();
 
 /* GET home page. */
-router.get('/', function(req, res, next) {
-  res.render('index', { title: 'Express' });
+router.get('/', (req, res, next) => {
+  res.render('index', { title: 'Express',});
 });
+
+router.get('/search/:query', (req, res, next) => {
+  let query = req.params.query;
+  request(searchUrl + '&q=' + query, (err, body) => {
+    return res.json(body);
+  });
+});
+
 
 module.exports = router;
