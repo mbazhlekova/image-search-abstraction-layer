50f16ee146d39c92a2e4a39665daa9f827da1de1
mbazhlekova <maria.bazhlekova@gmail.com>
2017-06-22 23:17:45 -0500

    added latest route

--- routes/index.js
+++ routes/index.js
@@ -3,6 +3,7 @@ dotenv.load();
 
 const api_key = process.env.API_KEY;
 const searchUrl = 'http://api.giphy.com/v1/gifs/search?api_key=' + api_key;
+const trendingUrl = 'http://api.giphy.com/v1/gifs/trending?api_key=' + api_key;
 
 var express = require('express');
 var request = require('request');
@@ -17,6 +18,12 @@ router.get('/', (req, res, next) => {
 router.get('/search/:query', (req, res, next) => {
   let query = req.params.query;
   request(searchUrl + '&q=' + query, (err, body) => {
+    return res.json(body);
+  });
+});
+
+router.get('/latest', (req, res, next) => {
+  request(trendingUrl, (err, body) => {
     return res.json(body);
   });
 });
